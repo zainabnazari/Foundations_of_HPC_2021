32d0ee7e51fc94b1264728fbad4a407eb880e9af
Zainab Nazari <nazari.zainab@gmail.com>
2021-12-28 16:48:09 +0100

    update matrix3d

--- Assignment1/sum3Dmatrix.cpp
+++ Assignment1/sum3Dmatrix.cpp
@@ -54,20 +54,38 @@ int main (int argc, char * argv[]) {
   MPI_Comm_size( MPI_COMM_WORLD,&size );
 r = ((double) rand() / (RAND_MAX));
 
-//std::cout << r << std::endl;
 int dim1, dim2, dim3;
+int in_dim1, in_dim2, in_dim3;
 
-dim1 = std::stoi(argv[1]);
-dim2 = std::stoi(argv[2]);
-dim3 = std::stoi(argv[3]);
+in_dim1 = std::stoi(argv[1]);
+in_dim2 = std::stoi(argv[2]);
+in_dim3 = std::stoi(argv[3]);
+dim1=in_dim1;
+dim2=in_dim2;
+dim3=in_dim3/size;
 
+int*** big_matrix=initialise_matrix3d(in_dim1, in_dim2, in_dim3);
 int*** a=initialise_matrix3d(dim1, dim2, dim3);
 int*** b=initialise_matrix3d(dim1,dim2,dim3);
 int*** c=addition3d(a, b, dim1, dim2, dim3 );
+int size_small_matrix=dim1*dim2*dim3;
+MPI_Gather(c,size_small_matrix,MPI_DOUBLE,big_matrix,size_small_matrix,MPI_DOUBLE,0,MPI_COMM_WORLD);
 delete3dmatrix(a, dim1, dim2);
 delete3dmatrix(b, dim1, dim2);
+delete3dmatrix(c, dim1, dim2);
+delete3dmatrix(big_matrix, in_dim1, in_dim2);
 
 std::cout << rank << std::endl;
+
+// Displaying the values with proper index.
+   for (int i = 0; i <dim1; ++i) {
+       for (int j = 0; j < dim2; ++j) {
+           for (int k = 0; k < dim3; ++k) {
+               std::cout << "c[" << i << "][" << j << "][" << k << "] = " << c[i][j][k] << std::endl;
+           }
+       }
+   }
+
 MPI_Finalize();
 return 0;
 
