8d15ebeb060c345e940b959cae2b6be8a16adf07
Zainab Nazari <nazari.zainab@gmail.com>
2021-12-29 20:45:21 +0100

    modifiedpbs.sh append

--- Assignment1/section2/gpu-results/pbs_gpu.sh
+++ Assignment1/section2/gpu-results/pbs_gpu.sh
@@ -38,33 +38,33 @@ mpirun -np 2 --report-bindings --map-by node --mca pml ucx --mca btl self,tcp --
 module purge 2>/dev/null
 module load intel 2>/dev/null
 
-mpiexec -np 2 -env I_MPI_DEBUG 5 -genv I_MPI_FABRICS=shm -genv  I_MPI_PIN_PROCESSOR_LIST 0,1  ./IMB-MPI1_intel PingPong -msglog 28 2>/dev/null | grep -v ^\# | grep -v '^$' | grep -v '^\[' | sed -r 's/  */,/g' | cut -d "," -f2- >>result_core_shm_intel.csv
+mpiexec -np 2 -env I_MPI_DEBUG 5 -genv I_MPI_FABRICS=shm -genv  I_MPI_PIN_PROCESSOR_LIST 0,1  ./IMB-MPI1_intel PingPong -msglog 28 2>/dev/null | grep -v ^\# | grep -v '^$' | grep -v '^\[' | sed -r 's/  */,/g' | cut -d "," -f2- >result_core_shm_intel.csv
 
 
-mpiexec -np 2 -env I_MPI_DEBUG 5 -genv I_MPI_FABRICS=shm -genv  I_MPI_PIN_PROCESSOR_LIST 0,2  ./IMB-MPI1_intel PingPong -msglog 28 2>/dev/null | grep -v ^\# | grep -v '^$' | grep -v '^\[' | sed -r 's/  */,/g' | cut -d "," -f2- >>result_socket_shm_intel.csv
+mpiexec -np 2 -env I_MPI_DEBUG 5 -genv I_MPI_FABRICS=shm -genv  I_MPI_PIN_PROCESSOR_LIST 0,2  ./IMB-MPI1_intel PingPong -msglog 28 2>/dev/null | grep -v ^\# | grep -v '^$' | grep -v '^\[' | sed -r 's/  */,/g' | cut -d "," -f2- >result_socket_shm_intel.csv
 
-mpiexec -np 2 -env I_MPI_DEBUG 5 -genv I_MPI_FABRICS=ofi -genv I_MPI_OFI_PROVIDER=tcp -genv I_MPI_PIN_PROCESSOR_LIST 0,1 ./IMB-MPI1_intel PingPong -msglog 28 2>/dev/null | grep -v ^\# | grep -v '^$' | grep -v '^\[' | sed -r 's/  */,/g' | cut -d "," -f2- >>result_core_ofi_tcp_intel.csv
-
-
-mpiexec -np 2 -env I_MPI_DEBUG 5 -genv I_MPI_FABRICS=ofi -genv I_MPI_OFI_PROVIDER=tcp -genv I_MPI_PIN_PROCESSOR_LIST 0,2 ./IMB-MPI1_intel PingPong -msglog 28 2>/dev/null | grep -v ^\# | grep -v '^$' | grep -v '^\[' | sed -r 's/  */,/g' | cut -d "," -f2- >>result_socket_ofi_tcp_intel.csv
+mpiexec -np 2 -env I_MPI_DEBUG 5 -genv I_MPI_FABRICS=ofi -genv I_MPI_OFI_PROVIDER=tcp -genv I_MPI_PIN_PROCESSOR_LIST 0,1 ./IMB-MPI1_intel PingPong -msglog 28 2>/dev/null | grep -v ^\# | grep -v '^$' | grep -v '^\[' | sed -r 's/  */,/g' | cut -d "," -f2- >result_core_ofi_tcp_intel.csv
 
 
-mpiexec -np 2 -env I_MPI_DEBUG 5 -genv I_MPI_FABRICS=ofi -genv I_MPI_OFI_PROVIDER=mlx -genv I_MPI_PIN_PROCESSOR_LIST 0,2 ./IMB-MPI1_intel PingPong -msglog 28 2>/dev/null | grep -v ^\# | grep -v '^$' | grep -v '^\[' | sed -r 's/  */,/g' | cut -d "," -f2- >>result_socket_ofi_mlx_intel.csv
+mpiexec -np 2 -env I_MPI_DEBUG 5 -genv I_MPI_FABRICS=ofi -genv I_MPI_OFI_PROVIDER=tcp -genv I_MPI_PIN_PROCESSOR_LIST 0,2 ./IMB-MPI1_intel PingPong -msglog 28 2>/dev/null | grep -v ^\# | grep -v '^$' | grep -v '^\[' | sed -r 's/  */,/g' | cut -d "," -f2- >result_socket_ofi_tcp_intel.csv
 
 
-mpiexec -np 2 -env I_MPI_DEBUG 5 -genv I_MPI_FABRICS=ofi -genv I_MPI_OFI_PROVIDER=mlx -genv I_MPI_PIN_PROCESSOR_LIST 0,1 ./IMB-MPI1_intel PingPong -msglog 28 2>/dev/null | grep -v ^\# | grep -v '^$' | grep -v '^\[' | sed -r 's/  */,/g' | cut -d "," -f2- >>result_core_ofi_mlx_intel.csv
-
-mpiexec -np 2 -env I_MPI_DEBUG 5 -genv I_MPI_FABRICS=ofi -genv I_MPI_OFI_PROVIDER=sockets -genv I_MPI_PIN_PROCESSOR_LIST 0,1 ./IMB-MPI1_intel PingPong -msglog 28 2>/dev/null | grep -v ^\# | grep -v '^$' | grep -v '^\[' | sed -r 's/  */,/g' | cut -d "," -f2- >>result_core_ofi_sockets_intel.csv
+mpiexec -np 2 -env I_MPI_DEBUG 5 -genv I_MPI_FABRICS=ofi -genv I_MPI_OFI_PROVIDER=mlx -genv I_MPI_PIN_PROCESSOR_LIST 0,2 ./IMB-MPI1_intel PingPong -msglog 28 2>/dev/null | grep -v ^\# | grep -v '^$' | grep -v '^\[' | sed -r 's/  */,/g' | cut -d "," -f2- >result_socket_ofi_mlx_intel.csv
 
 
-mpiexec -np 2 -env I_MPI_DEBUG 5 -genv I_MPI_FABRICS=ofi -genv I_MPI_OFI_PROVIDER=sockets -genv I_MPI_PIN_PROCESSOR_LIST 0,2 ./IMB-MPI1_intel PingPong -msglog 28 2>/dev/null | grep -v ^\# | grep -v '^$' | grep -v '^\[' | sed -r 's/  */,/g' | cut -d "," -f2- >>result_socket_ofi_sockets_intel.csv
+mpiexec -np 2 -env I_MPI_DEBUG 5 -genv I_MPI_FABRICS=ofi -genv I_MPI_OFI_PROVIDER=mlx -genv I_MPI_PIN_PROCESSOR_LIST 0,1 ./IMB-MPI1_intel PingPong -msglog 28 2>/dev/null | grep -v ^\# | grep -v '^$' | grep -v '^\[' | sed -r 's/  */,/g' | cut -d "," -f2- >result_core_ofi_mlx_intel.csv
 
-mpiexec -np 2 -ppn=1 -env I_MPI_DEBUG 5 -genv I_MPI_FABRICS=ofi -genv I_MPI_OFI_PROVIDER=tcp ./IMB-MPI1_intel PingPong -msglog 28 2>/dev/null | grep -v ^\# | grep -v '^$' | grep -v '^\[' | sed -r 's/  */,/g' | cut -d "," -f2-  | tail -n +3 >>result_node_ofi_tcp_intel.csv
-
-
-mpiexec -np 2 -ppn=1 -env I_MPI_DEBUG 5 -genv I_MPI_FABRICS=ofi -genv I_MPI_OFI_PROVIDER=mlx ./IMB-MPI1_intel PingPong -msglog 28 2>/dev/null | grep -v ^\# | grep -v '^$' | grep -v '^\[' | sed -r 's/  */,/g' | cut -d "," -f2-  | tail -n +3 >>result_node_ofi_mlx_intel.csv
+mpiexec -np 2 -env I_MPI_DEBUG 5 -genv I_MPI_FABRICS=ofi -genv I_MPI_OFI_PROVIDER=sockets -genv I_MPI_PIN_PROCESSOR_LIST 0,1 ./IMB-MPI1_intel PingPong -msglog 28 2>/dev/null | grep -v ^\# | grep -v '^$' | grep -v '^\[' | sed -r 's/  */,/g' | cut -d "," -f2- >result_core_ofi_sockets_intel.csv
 
 
-mpiexec -np 2 -ppn=1 -env I_MPI_DEBUG 5 -genv I_MPI_FABRICS=ofi -genv I_MPI_OFI_PROVIDER=sockets ./IMB-MPI1_intel PingPong -msglog 28 2>/dev/null | grep -v ^\# | grep -v '^$' | grep -v '^\[' | sed -r 's/  */,/g' | cut -d "," -f2- | tail -n +3 >>result_node_ofi_sockets_intel.csv
+mpiexec -np 2 -env I_MPI_DEBUG 5 -genv I_MPI_FABRICS=ofi -genv I_MPI_OFI_PROVIDER=sockets -genv I_MPI_PIN_PROCESSOR_LIST 0,2 ./IMB-MPI1_intel PingPong -msglog 28 2>/dev/null | grep -v ^\# | grep -v '^$' | grep -v '^\[' | sed -r 's/  */,/g' | cut -d "," -f2- >result_socket_ofi_sockets_intel.csv
+
+mpiexec -np 2 -ppn=1 -env I_MPI_DEBUG 5 -genv I_MPI_FABRICS=ofi -genv I_MPI_OFI_PROVIDER=tcp ./IMB-MPI1_intel PingPong -msglog 28 2>/dev/null | grep -v ^\# | grep -v '^$' | grep -v '^\[' | sed -r 's/  */,/g' | cut -d "," -f2-  | tail -n +3 >result_node_ofi_tcp_intel.csv
+
+
+mpiexec -np 2 -ppn=1 -env I_MPI_DEBUG 5 -genv I_MPI_FABRICS=ofi -genv I_MPI_OFI_PROVIDER=mlx ./IMB-MPI1_intel PingPong -msglog 28 2>/dev/null | grep -v ^\# | grep -v '^$' | grep -v '^\[' | sed -r 's/  */,/g' | cut -d "," -f2-  | tail -n +3 >result_node_ofi_mlx_intel.csv
+
+
+mpiexec -np 2 -ppn=1 -env I_MPI_DEBUG 5 -genv I_MPI_FABRICS=ofi -genv I_MPI_OFI_PROVIDER=sockets ./IMB-MPI1_intel PingPong -msglog 28 2>/dev/null | grep -v ^\# | grep -v '^$' | grep -v '^\[' | sed -r 's/  */,/g' | cut -d "," -f2- | tail -n +3 >result_node_ofi_sockets_intel.csv
 
 
